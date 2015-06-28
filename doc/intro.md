6c59f238868678dd2a204c257cb6ca635133c6ed
Manuel Odendahl <wesen@ruinwesen.com>
2015-06-27 21:57:21 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-pbrt
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_pbrt/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-pbrt.core-test
+  (:require [clojure.test :refer :all]
+            [clj-pbrt.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
